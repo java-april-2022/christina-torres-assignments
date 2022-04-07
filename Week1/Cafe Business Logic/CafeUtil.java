a02e94618d0bd0fdb4269b79f4c18f01f2754302
xtina-lt <xtina_lt@outlook.com>
2022-04-07 13:04:55 -0400

    update for instances

--- Week1/Cafe Business Logic/CafeUtil.java
+++ Week1/Cafe Business Logic/CafeUtil.java
@@ -1,44 +1,47 @@
 import java.util.*;
+
 class CafeUtil {
-    // CafeUtil(){
+    double result;
 
-    // }
+    CafeUtil(){
+        result = 0;
+    }
 
-    static Integer getStreakGoal(int numWeeks){
-        int result = 0;
-        for (int i = 1; i < numWeeks+1; i++){
-            result += i;
+    Integer getStreakGoal(int e){
+        int resultCast = (int)result;
+        for (int i = 1; i < e+1; i++){
+            resultCast += i;
         }
-        return result;
+        return resultCast;
     }
 
-    static double getOrderTotal(double[] prices){
+    double getOrderTotal(double[] e){
         double result = 0;
-        for (double i : prices) result += i;
+        for (double i : e) result += i;
         return result;
     }
 
-    static void displayMenu(){
+    void displayMenu(){
         ArrayList<String> menu = new ArrayList<String>();
         menu.add("Drip Coffee");
         menu.add("Cappuccino");
         menu.add("Latte");
         menu.add("Mocha");
-        for (String i : menu) System.out.printf("%d %s\n", menu.indexOf(i), i);
+        for (String i : menu) System.out.printf("%d - %s\n", menu.indexOf(i), i);
     }
 
-    static void printPriceChart(String product, double price, int maxQuantity){
+    void printPriceChart(String product, double price, int maxQuantity){
         ArrayList<Double> priceChart = new ArrayList<Double>();
         for (int i = 1; i <= maxQuantity; i++) priceChart.add(price * i);
         System.out.println(product);
         for(Double i : priceChart) System.out.printf("%d - $%.2f\n", priceChart.indexOf(i), i);
     }
 
-    static void addCustomer(String e){
-        ArrayList<String> customers = new ArrayList<String>();
-        customers.add(e);
-        System.out.printf("Hello, %s\n", e.toUpperCase());
+    static void addCustomer(ArrayList<String> e){
+        System.out.println("New Customers:");
+        for (String i : e) System.out.println( i.toUpperCase() );
     }
+
     public static void main(String[] args){
     }
 }
